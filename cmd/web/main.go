47c350ac2cb922ec0ae364a512bf9b7b2f8665de
Travis Aucoin <travisdevaucoin@gmail.com>
2023-11-22 22:48:20 -0600

    feat: added structured logger

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,29 @@
 package main
 
 import (
-	"log"
+	"flag"
+	"log/slog"
 	"net/http"
+	"os"
 )
 
+type application struct {
+	logger *slog.Logger
+}
+
 func main() {
+	// Define application flags
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
+	// Setup structured logging
+	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		AddSource: true,
+	}))
+	// Initialize a new instance of application containing the dependencies.
+	app := &application{
+		logger: logger,
+	}
+
 	mux := http.NewServeMux()
 	// Create a file server which serves files out of the "./ui/static" directory.
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
@@ -13,12 +31,13 @@ func main() {
 	// for all URL paths that start with "/static/". For matching paths, we strip
 	// the "/static" prefix before the request reaches the file server.
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet/view", snippetView)
-	mux.HandleFunc("/snippet/create", snippetCreate)
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/snippet/view", app.snippetView)
+	mux.HandleFunc("/snippet/create", app.snippetCreate)
 
-	log.Print("starting server on :4000")
+	logger.Info("starting server", "addr", *addr)
 
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+	err := http.ListenAndServe(*addr, mux)
+	logger.Error(err.Error())
+	os.Exit(1)
 }
