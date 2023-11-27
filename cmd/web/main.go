b81ed94c2eb8be59103d30d6913a034b27f6ca14
Travis Aucoin <travisdevaucoin@gmail.com>
2023-11-26 22:29:08 -0800

    refactor: split routes out of main

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -24,20 +24,9 @@ func main() {
 		logger: logger,
 	}
 
-	mux := http.NewServeMux()
-	// Create a file server which serves files out of the "./ui/static" directory.
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	// Use the mux.Handle() function to register the file server as the handler
-	// for all URL paths that start with "/static/". For matching paths, we strip
-	// the "/static" prefix before the request reaches the file server.
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet/view", app.snippetView)
-	mux.HandleFunc("/snippet/create", app.snippetCreate)
-
 	logger.Info("starting server", "addr", *addr)
 
-	err := http.ListenAndServe(*addr, mux)
+	err := http.ListenAndServe(*addr, app.routes())
 	logger.Error(err.Error())
 	os.Exit(1)
 }
---
+++ cmd/web/routes.go
@@ -0,0 +1,23 @@
+package main
+
+import "net/http"
+
+func (app *application) routes() *http.ServeMux {
+	mux := http.NewServeMux()
+
+	// Serves static files from the ui/static directory.
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+	// Use the mux.Handle() function to register the file server as the handler
+	// for all URL paths that start with "/static/". For matching paths, we strip
+	// the "/static" prefix before the request reaches the file server.
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	// Register the relevant methods, URL patterns and handler functions for our
+	// endpoints using the mux.HandleFunc() method.
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/snippet/view", app.snippetView)
+	mux.HandleFunc("/snippet/create", app.snippetCreate)
+
+	return mux
+}
